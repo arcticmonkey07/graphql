dd85aa8d59eae13f3092f5e3b19e8a787dba4272
arcticmonkey07 <arcticmonkey07@gmail.com>
2019-10-07 00:34:10 +0300

    Adding Elements to DB

--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -1,6 +1,18 @@
 import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'recompose';
-
+import {graphql} from "react-apollo/index";
+import {addMovieMutation} from "./mutations";
+import {moviesQuery} from "../MoviesTable/queries";
 import { styles } from './styles';
+import {directorsQuery} from "./queries";
 
-export default compose(withStyles(styles));
+const withGraphqlAdd = graphql(addMovieMutation, {
+  props: ({ mutate }) => ({
+    addMovie: movie => mutate({
+      variables: movie,
+      refetchQueries: [{ query: moviesQuery }],
+    }),
+  }),
+});
+
+export default compose(withStyles(styles), withGraphqlAdd, graphql(directorsQuery));
--- server/server/app.js
+++ server/server/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
 
 mongoose.connect(`mongodb+srv://arctic:2130945@arctic-a7ck0.gcp.mongodb.net/graphql?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
